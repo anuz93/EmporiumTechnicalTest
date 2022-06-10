d979986c263a5a69b45d544ce0c843eee4a05d70
anuz93 <107268096+anuz93@users.noreply.github.com>
2022-06-10 16:54:39 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const nav = document.querySelector(".navigation");
+const burger = document.querySelector('.burger');
+
+burger.addEventListener("click", ()=>{
+    nav.classList.toggle("show-nav")
+})
+
